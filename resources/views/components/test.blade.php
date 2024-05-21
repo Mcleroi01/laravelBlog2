099e482cc8783ef6ec3ff75013218a0ae76a99d9
Musongela <musongelacarlo54@gmail.com>
2024-05-21 18:05:04 +0100

    relationtbale

---
+++ resources/views/components/test.blade.php
@@ -0,0 +1,11 @@
+@props(['class'])
+
+
+<div class="{{ $class }}">
+    <div>{{ $title }}</div>
+    <div class="bg-green-800 text-slate-50 p-4">
+
+        {{ $slot }}
+        
+    </div>
+</div>
